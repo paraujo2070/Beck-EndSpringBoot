26b45f9c25108f2779180c5b8b7f6d92f538f530
Pedro Antonio <paraujo20700@gmail.com>
2025-02-24 20:56:22 -0300

    Projeto inicial Spring Boot

---
+++ PlanejamentoFinanceiro/src/main/java/com/pedroantonio/PlanejamentoFinanceiro/PlanejamentoFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.pedroantonio.PlanejamentoFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanejamentoFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanejamentoFinanceiroApplication.class, args);
+	}
+
+}
---
+++ PlanejamentoFinanceiro/src/test/java/com/pedroantonio/PlanejamentoFinanceiro/PlanejamentoFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroantonio.PlanejamentoFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanejamentoFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
